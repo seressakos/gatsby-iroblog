17b6db8d8373e67ac18e6a079203a25601a87969
Akos Seress <akos.seress@pronovix.com>
2022-01-07 16:08:32 +0100

    Initial commit

---
+++ iroblog/src/components/layout.js
@@ -0,0 +1,78 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+  allNodeHomePage {
+    nodes {
+      field_hero {
+        width
+      }
+      relationships {
+        field_hero {
+          uri {
+            url
+          }
+        }
+        field_bookimage {
+          uri {
+            url
+          }
+        }
+        field_profile_image {
+          uri {
+            url
+          }
+        }
+      }
+      field_booktext {
+        value
+      }
+    }
+  }
+}
+  `)
+
+  console.log(data)
+
+  return (
+    <>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
